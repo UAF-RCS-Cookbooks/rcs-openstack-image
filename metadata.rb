3f6e868705d0ffb79285c9cc8160af63dc9f542e
Jeremiah Dabney <jdabney@gmail.com>
2025-04-02 21:48:47 -0800

    Removed version locks

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Installs and configures the Glance Image Registry and Delivery Service'
-version          '20.0.4'
+version          '20.0.5'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'rcs-openstackclient'
-depends 'rcs-openstack-common', '>= 20.0.0'
-depends 'rcs-openstack-identity', '>= 20.0.0'
+depends 'rcs-openstack-common'
+depends 'rcs-openstack-identity'
 
 chef_version '>= 16.0'
